28b0879e2c4d345406a0d9b9d00b7ce2bd4685e1
MarkoDragas <121483118+MarkoDragas@users.noreply.github.com>
2022-12-31 11:55:11 +0100

    Add inputId and submitButton variable

--- cobrafunnel.js
+++ cobrafunnel.js
@@ -1,5 +1,4 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-
 const firebaseConfig = {
     apiKey: "AIzaSyBq1gWVZFLNPS4KmdcYUCQ1OQ-9buLyF3E",
     authDomain: "cobrafunnel.firebaseapp.com",
@@ -10,29 +9,37 @@ const firebaseConfig = {
     appId: "1:65623425643:web:99afba5ae374b10a1d672e"
 };
 
-        // Initialize Firebas
+
 const app = initializeApp(firebaseConfig);
-        var desktopMAIL = document.getElementById("mailbutton");
+var desktopMAIL = document.getElementById("mailbutton");
 
 import {getDatabase, ref, set} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 
 const database = getDatabase();
 
+var inputId;
+var submitButton;
+
 function sendEmail(xid, yid) {
     var mailko = document.getElementById(xid);
     var gumbic = document.getElementById(yid);
-        const d = new Date();
-        var datum = d.getFullYear().toString() + d.getMonth().toString() + d.getDate().toString() + d.getHours().toString() + d.getMinutes().toString() + d.getSeconds().toString();    
-        set(ref(database, 'emails/' + datum), {
+    const d = new Date();
+    var datum = d.getFullYear().toString() + d.getMonth().toString() + d.getDate().toString() + d.getHours().toString() + d.getMinutes().toString() + d.getSeconds().toString();    
+    set(ref(database, 'emails/' + datum), {
         email: mailko.value
-        }).then(()=>{
+    }).then(()=>{
         gumbic.innerHTML = "Success!";
         mailko.value = "";
-        }).catch((error) => {
+    }).catch((error) => {
         gumbic.style.background = "#ff3f38";
         mailko.value = "";
         gumbic.innerHTML = "An error has occured.";
-        })
+    })
 }
-        
-desktopMAIL.addEventListener('click', () => sendEmail("email_input", "mailbutton"));
+
+if (inputId && submitButton == null) {
+    inputId = "email_input";
+    submitButton = "mailbutton";
+}
+
+desktopMAIL.addEventListener('click', () => sendEmail(inputId, submitButton));
